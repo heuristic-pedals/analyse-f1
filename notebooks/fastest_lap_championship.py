51e430c8f04c49375bf94f4631729bb22cfdea55
Heuristic Pedals <heuristicpedals@gmail.com>
2025-05-29 18:13:09 +0100

    feat: added constructors

--- notebooks/fastest_lap_championship.py
+++ notebooks/fastest_lap_championship.py
@@ -110,7 +110,7 @@ def mode(x):
     return x.mode().iloc[0] if not x.mode().empty else None
 
 
-standings = (
+drivers_standings = (
     results.groupby("Driver", as_index=False)
     .agg(
         {
@@ -125,9 +125,16 @@ standings = (
     .reset_index(drop=True)
 )
 
+constructors_standings = (
+    results.groupby("TeamName", as_index=False)
+    .agg({"FastestLapPoints": "sum", "TeamColor": mode})
+    .sort_values("FastestLapPoints", ascending=False)
+    .reset_index(drop=True)
+)
+
 # %%
 # calculate the number of occurances
-countback = (
+driver_countback = (
     results.groupby("Driver", as_index=False)["Position"]
     .value_counts()
     .rename(columns={"count": "PositionAchievements"})
@@ -135,22 +142,50 @@ countback = (
     .fillna(0)
 )
 # zfill to set valid sort order + make Driver col
-countback.columns = [
+driver_countback.columns = [
     "Position" + str(col).zfill(2)
-    for col in countback.columns.get_level_values(0)
+    for col in driver_countback.columns.get_level_values(0)
 ]
-countback.reset_index(inplace=True)
+driver_countback.reset_index(inplace=True)
 
 # merge countback and sort standing by points and countback occurances
-standings = standings.merge(countback, on="Driver")
-countback_columns = list(countback.columns)
-countback_columns.remove("Driver")
-standings.sort_values(
-    ["FastestLapPoints"] + sorted(countback_columns),
+drivers_standings = drivers_standings.merge(driver_countback, on="Driver")
+driver_countback_columns = list(driver_countback.columns)
+driver_countback_columns.remove("Driver")
+drivers_standings.sort_values(
+    ["FastestLapPoints"] + sorted(driver_countback_columns),
     inplace=True,
     ascending=False,
 )
-standings.reset_index(drop=True, inplace=True)
+drivers_standings.reset_index(drop=True, inplace=True)
+
+# %%
+constructors_countback = (
+    results.groupby("TeamName", as_index=False)["Position"]
+    .value_counts()
+    .rename(columns={"count": "PositionAchievements"})
+    .pivot(columns="Position", index="TeamName", values="PositionAchievements")
+    .fillna(0)
+)
+# zfill to set valid sort order + make Driver col
+constructors_countback.columns = [
+    "Position" + str(col).zfill(2)
+    for col in constructors_countback.columns.get_level_values(0)
+]
+constructors_countback.reset_index(inplace=True)
+
+# merge countback and sort standing by points and countback occurances
+constructors_standings = constructors_standings.merge(
+    constructors_countback, on="TeamName"
+)
+constructors_countback_columns = list(constructors_countback.columns)
+constructors_countback_columns.remove("TeamName")
+constructors_standings.sort_values(
+    ["FastestLapPoints"] + sorted(constructors_countback_columns),
+    inplace=True,
+    ascending=False,
+)
+constructors_standings.reset_index(drop=True, inplace=True)
 
 # %%
 hp_template = go.layout.Template(
@@ -208,7 +243,7 @@ x_pixel_adjust = 1e-1
 hp_red = "#C8102E"
 hp_white = "#FFFFFF"
 hp_black = "#000000"
-xlarge_font = 24
+xlarge_font = 23
 large_font = 20
 med_font = 16
 small_font = 14
@@ -267,12 +302,12 @@ fig.add_annotation(
 )
 
 # get starting location and adjust axes
-grid_line_y = len(standings) * v_spacing
+grid_line_y = len(drivers_standings) * v_spacing
 fig.update_xaxes(range=[-h_padding, 2 * grid_width + h_spacing + h_padding])
 fig.update_yaxes(range=[-v_spacing, grid_line_y + v_spacing])
 
 
-for i, driver in standings.iterrows():
+for i, driver in drivers_standings.iterrows():
     # calculate the column
     col = i % 2
 
@@ -390,7 +425,220 @@ for i, driver in standings.iterrows():
     grid_line_y -= v_spacing
 
 fig.write_image(
-    f"../outputs/drivers_fastes_laps_standings_{num_races}_rounds.png"
+    f"../outputs/drivers_fastest_laps_standings_{num_races}_rounds.png"
+)
+fig.show()
+
+# %%
+fig = go.Figure()
+fig.update_layout(
+    template=hp_template,
+    width=600,
+    height=575,
+)
+
+v_spacing = 5
+h_spacing = 2
+h_padding = 1
+grid_width = 10
+grid_thick_vert = 2
+grid_thin_vert = 5
+
+# line widths
+thick_line = 5
+think_line = 3
+
+# adjustments
+x_pixel_adjust = 1e-1
+
+# hp styling
+hp_red = "#C8102E"
+hp_white = "#FFFFFF"
+hp_black = "#000000"
+xlarge_font = 23
+large_font = 20
+med_font = 16
+small_font = 14
+
+# data locations
+position_num = 0.11
+name = 0.2
+pts = 0.94
+
+# update the title
+fig.update_layout(
+    title=dict(  # noqa: C408
+        text="<b>2025 Constructors Fastest Lap Championship<b>",
+        font=dict(size=xlarge_font),  # noqa: C408
+        y=0.945,
+    )
+)
+
+# update the subtitle
+fig.add_annotation(
+    text=(
+        f"Standings after {num_races} rounds (post {last_race} on "
+        f"{last_race_date})."
+    ),
+    xanchor="left",
+    yref="paper",
+    align="left",
+    x=-2 * x_pixel_adjust,
+    y=1.035,
+    showarrow=False,
+    font=dict(size=small_font),  # noqa: C408
+)
+
+# add sources and confidence
+fig.add_annotation(
+    text=(
+        "Sources: F1 API/FastF1 v3.5.3 (collection), "
+        "Grid Graphs (analysis and visualisation)."
+    ),
+    x=-x_pixel_adjust,
+    y=-grid_thin_vert,
+    xanchor="left",
+    align="center",
+    showarrow=False,
+    font=dict(color="darkgrey", size=10),  # noqa: C408
+)
+fig.add_annotation(
+    text="High Confidence",
+    x=2 * grid_width + h_spacing,
+    y=-grid_thin_vert,
+    xanchor="right",
+    showarrow=False,
+    font=dict(color="white", size=10),  # noqa: C408
+    bgcolor="#008000",
+    bordercolor="black",
+    borderpad=2,
+)
+
+# get starting location and adjust axes
+grid_line_y = len(constructors_standings) * v_spacing
+fig.update_xaxes(range=[-h_padding, 2 * grid_width + h_spacing + h_padding])
+fig.update_yaxes(range=[-v_spacing, grid_line_y + v_spacing])
+
+
+for i, constructor in constructors_standings.iterrows():
+    # calculate the column
+    col = i % 2
+
+    # main grid line
+    fig.add_shape(
+        type="line",
+        x0=col * (grid_width + h_spacing),
+        x1=grid_width + (col * (grid_width + h_spacing)),
+        y0=grid_line_y,
+        y1=grid_line_y,
+        line=dict(color="black", width=thick_line),  # noqa: C408
+    )
+
+    # vertical LHS of grid box (thick and thin sections)
+    fig.add_shape(
+        type="line",
+        x0=col * (grid_width + h_spacing) + x_pixel_adjust,
+        x1=col * (grid_width + h_spacing) + x_pixel_adjust,
+        xanchor="right",
+        y0=grid_line_y,
+        y1=grid_line_y - grid_thick_vert,
+        line=dict(color="black", width=thick_line),  # noqa: C408
+    )
+    fig.add_shape(
+        type="line",
+        x0=col * (grid_width + h_spacing) + x_pixel_adjust,
+        x1=col * (grid_width + h_spacing) + x_pixel_adjust,
+        y0=grid_line_y,
+        y1=grid_line_y - grid_thin_vert,
+        line=dict(color="black", width=think_line),  # noqa: C408
+    )
+
+    # vertical RHS of grid box (thick and thin sections)
+    fig.add_shape(
+        type="line",
+        x0=grid_width + (col * (grid_width + h_spacing)) - x_pixel_adjust,
+        x1=grid_width + (col * (grid_width + h_spacing)) - x_pixel_adjust,
+        xanchor="right",
+        y0=grid_line_y,
+        y1=grid_line_y - grid_thick_vert,
+        line=dict(color="black", width=thick_line),  # noqa: C408
+    )
+    fig.add_shape(
+        type="line",
+        x0=grid_width + (col * (grid_width + h_spacing)) - x_pixel_adjust,
+        x1=grid_width + (col * (grid_width + h_spacing)) - x_pixel_adjust,
+        y0=grid_line_y,
+        y1=grid_line_y - grid_thin_vert,
+        line=dict(color="black", width=think_line),  # noqa: C408
+    )
+
+    # add position number
+    pos_num_x = (col * (grid_width + h_spacing) + x_pixel_adjust) + (
+        position_num * grid_width
+    )
+    text_y = grid_line_y - (grid_thin_vert / 2)
+    fig.add_shape(
+        type="rect",
+        x0=pos_num_x - (6 * position_num),
+        x1=pos_num_x + (6 * position_num),
+        xanchor="center",
+        y0=grid_line_y - (0.15 * grid_thin_vert),
+        y1=grid_line_y - (0.85 * grid_thin_vert),
+        yanchor="middle",
+        line=dict(width=2, color="Black"),  # noqa: C408
+        fillcolor=hp_red,
+    )
+    fig.add_annotation(
+        text=i + 1,
+        font=dict(size=large_font, color=hp_white),  # noqa: C408
+        x=pos_num_x,
+        y=text_y,
+        xanchor="center",
+        yanchor="middle",
+        align="center",
+        showarrow=False,
+    )
+
+    # team name
+    name_x = (col * (grid_width + h_spacing) + x_pixel_adjust) + (
+        name * grid_width
+    )
+    fig.add_annotation(
+        text=constructor["TeamName"],
+        font=dict(size=med_font, color=f"#{constructor['TeamColor']}"),  # noqa: C408
+        x=name_x,
+        y=text_y,
+        xanchor="left",
+        yanchor="middle",
+        align="center",
+        showarrow=False,
+    )
+
+    # points
+    points_x = (col * (grid_width + h_spacing) + x_pixel_adjust) + (
+        pts * grid_width
+    )
+    pts_value = constructor["FastestLapPoints"]
+    fig.add_annotation(
+        text=(
+            f"{pts_value:.0f}pts"
+            if pts_value.is_integer()
+            else f"{pts_value:.1f}pts"
+        ),
+        font=dict(size=med_font, color=hp_black),  # noqa: C408
+        x=points_x,
+        y=text_y,
+        xanchor="right",
+        yanchor="middle",
+        align="center",
+        showarrow=False,
+    )
+
+    # move onto the next grid line
+    grid_line_y -= v_spacing
+
+fig.write_image(
+    f"../outputs/constructors_fastest_laps_standings_{num_races}_rounds.png"
 )
 fig.show()
 
